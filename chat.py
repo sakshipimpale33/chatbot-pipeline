969ff0985f06a526b9321400ebc93d8c051f8332
sakshi pimpale <sakshipimpale88@gmail.com>
2025-05-02 19:12:51 +0530

    new chat1.py

--- chat.py
+++ chat.py
@@ -1,8 +1,5 @@
+# chatbot.py (CI version)
 def chatbot_response():
-    return "Hello from chatbot!"
-
-if __name__ == "__main__":
-    print(chatbot_response())def chatbot_response():
     return "Hello from chatbot!"
 
 if __name__ == "__main__":
